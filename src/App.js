78110d6b60662ceecef7edb75d63d1ee03408ffc
Surekha <surekha@Surekhas-MacBook-Air.local>
2020-07-08 10:41:49 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AutoCompletedText from './AutoCompletedText.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      languages: [
+        { name: "Python", votes: 0 },
+        { name: "Java", votes: 0 },
+        { name: "React", votes: 0 },
+        { name: "Angular", votes: 0 }
+      ]
+    }
+  }
+  vote(i) {
+    let newLanguages = [...this.state.languages];
+    newLanguages[i].votes++;
+    this.setState({ languages: newLanguages });
+  }
+  render() {
+    return (
+      <>
+        <h1>Voting Application</h1>
+        <div className="languages">
+          {
+            this.state.languages.map((lang, i) =>
+              <div key={i} className="language">
+                <div className="voteCount">
+                  {lang.votes}
+                </div>
+                <div className="languageName">
+                  {lang.name}
+                </div>
+                <button onClick={this.vote.bind(this, i)}>Click Here</button>
+              </div>
+            )
+          }
+        </div>
+        <AutoCompletedText />
+      </>
+    );
+  }
 }
 
 export default App;
